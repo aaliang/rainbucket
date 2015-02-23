db01d9b301e73ac1e7aaf0d1286efeb8dafbdc8e
Andy Liang <a.liang1989@gmail.com>
2015-02-23 18:16:02 -0500

    Update nav-bar.js

--- src/main/resources/public/static/components/nav-bar/nav-bar.js
+++ src/main/resources/public/static/components/nav-bar/nav-bar.js
@@ -1,12 +1,6 @@
 define(['knockout', 'text!./nav-bar.html'], function(ko, template) {
 
   function NavBarViewModel(params) {
-    tellerum.vm.navBarViewModel = this; //add to vm locator
-
-    // This viewmodel doesn't do anything except pass through the 'route' parameter to the view.
-    // You could remove this viewmodel entirely, and define 'nav-bar' as a template-only component.
-    // But in most apps, you'll want some viewmodel logic to determine what navigation options appear.
-
     this.route = params.route;
   }
 
